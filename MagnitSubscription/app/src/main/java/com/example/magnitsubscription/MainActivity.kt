2e907b350c8271cfbee7327540a434e0dbb99287
Dmitry Kravchuk <jogerwarlock@yandex.ru>
2021-04-25 19:12:31 +0300

    Версия с фрагментами

---
+++ MagnitSubscription/app/src/main/java/com/example/magnitsubscription/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.magnitsubscription
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.example.magnitsubscription.databinding.ActivityMainBinding
+import com.example.magnitsubscription.fragments.HomeFragment
+import com.example.magnitsubscription.fragments.SubsFragment
+import com.example.magnitsubscription.fragments.UsrFragment
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        val homeFragment: Fragment = HomeFragment()
+        val subsFragment: Fragment = SubsFragment()
+        val usrFragment: Fragment = UsrFragment()
+
+        makeCurrentFragment(homeFragment)
+        bindingClass.btmNv.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menItHome -> makeCurrentFragment(homeFragment)
+                R.id.menItSubs -> makeCurrentFragment(subsFragment)
+                R.id.menItUsr -> makeCurrentFragment(usrFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace(R.id.frmLyt, fragment)
+        commit()
+    }
+}
---
+++ MagnitSubscription/app/src/main/java/com/example/magnitsubscription/constraints/Constraints.kt
@@ -0,0 +1,5 @@
+package com.example.magnitsubscription.constraints
+
+object Constraints {
+
+}
