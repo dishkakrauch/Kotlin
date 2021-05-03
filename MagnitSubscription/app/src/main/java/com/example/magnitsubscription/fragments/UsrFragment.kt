1e487b68bd58d5192092f3881d6648c0e3f910ee
Dmitry Kravchuk <jogerwarlock@yandex.ru>
2021-05-03 18:24:04 +0300

    Закончил страницу с профилем клиента

--- MagnitSubscription/app/src/main/java/com/example/magnitsubscription/fragments/UsrFragment.kt
+++ MagnitSubscription/app/src/main/java/com/example/magnitsubscription/fragments/UsrFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.example.magnitsubscription.R
+import com.google.android.material.snackbar.Snackbar
 
 class UsrFragment : Fragment() {
 
@@ -14,6 +17,35 @@ class UsrFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usr, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_usr, container, false)
+        val tvSupport: View = view.findViewById(R.id.tvSupport)
+        val tvNotif: View = view.findViewById(R.id.tvNotif)
+        val tvInfo: View = view.findViewById(R.id.tvInfo)
+        val tvEx: View = view.findViewById(R.id.tvEx)
+        val imvEdt: View = view.findViewById(R.id.imvEdt)
+
+        tvSupport.setOnClickListener {
+            findNavController().navigate(R.id.action_usrFragment_to_supportFragment)
+        }
+
+        tvNotif.setOnClickListener {
+            findNavController().navigate(R.id.action_usrFragment_to_notificationFragment)
+        }
+
+        tvInfo.setOnClickListener {
+            findNavController().navigate(R.id.action_usrFragment_to_infoFragment)
+        }
+
+        tvEx.setOnClickListener { view ->
+            Snackbar.make(view, R.string.dev, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        imvEdt.setOnClickListener { view ->
+            Snackbar.make(view, R.string.dev, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        return view
     }
 }
