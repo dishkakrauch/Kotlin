522eeb66c30b7aca3db6e22a52c82139228271ee
Dmitry Kravchuk <jogerwarlock@yandex.ru>
2021-05-05 22:35:50 +0300

    Дополнил второй лист описанием подписок

--- MagnitSubscription/app/src/main/java/com/example/magnitsubscription/fragments/SubsFragment.kt
+++ MagnitSubscription/app/src/main/java/com/example/magnitsubscription/fragments/SubsFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.example.magnitsubscription.R
+import com.google.android.material.snackbar.Snackbar
 
 class SubsFragment : Fragment() {
 
@@ -14,6 +19,50 @@ class SubsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subs, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_subs, container, false)
+        val btnAdr: Button = view.findViewById(R.id.btnAdr)
+        val imVwFml: ImageView = view.findViewById(R.id.imVwFml)
+        val tvFml: TextView = view.findViewById(R.id.tvFml)
+        val imVwOpt: ImageView = view.findViewById(R.id.imVwOpt)
+        val tvOpt: TextView = view.findViewById(R.id.tvOpt)
+        val imVwBsc: ImageView = view.findViewById(R.id.imVwBsc)
+        val tvBsc: TextView = view.findViewById(R.id.tvBsc)
+        val imVwInd: ImageView = view.findViewById(R.id.imVwInd)
+        val tvInd: TextView = view.findViewById(R.id.tvInd)
+
+        imVwFml.setOnClickListener {
+            findNavController().navigate(R.id.action_subsFragment_to_subs1Fragment)
+        }
+        tvFml.setOnClickListener {
+            findNavController().navigate(R.id.action_subsFragment_to_subs1Fragment)
+        }
+
+        imVwOpt.setOnClickListener {
+            findNavController().navigate(R.id.action_subsFragment_to_subs2Fragment)
+        }
+        tvOpt.setOnClickListener {
+            findNavController().navigate(R.id.action_subsFragment_to_subs2Fragment)
+        }
+
+        imVwBsc.setOnClickListener {
+            findNavController().navigate(R.id.action_subsFragment_to_subs3Fragment)
+        }
+        tvBsc.setOnClickListener {
+            findNavController().navigate(R.id.action_subsFragment_to_subs3Fragment)
+        }
+
+        imVwInd.setOnClickListener {
+            findNavController().navigate(R.id.action_subsFragment_to_subs4Fragment)
+        }
+        tvInd.setOnClickListener {
+            findNavController().navigate(R.id.action_subsFragment_to_subs4Fragment)
+        }
+
+        btnAdr.setOnClickListener { view ->
+            Snackbar.make(view, R.string.dev, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        return view
     }
 }
